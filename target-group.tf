96456f7558a27900504efaa1e00670fcd987556b
nsaxelby <mail@nsaxelby.com>
2023-06-09 23:11:47 +0100

    Proxy proto

--- target-group.tf
+++ target-group.tf
@@ -1,7 +1,8 @@
 resource "aws_lb_target_group" "my-target-group" {
-  name        = "my-target-group"
-  port        = 8888
-  protocol    = "TCP"
-  target_type = "ip"
-  vpc_id      = aws_vpc.main.id
+  name              = "my-target-group"
+  port              = 8888
+  protocol          = "TCP"
+  target_type       = "ip"
+  vpc_id            = aws_vpc.main.id
+  proxy_protocol_v2 = true
 }
