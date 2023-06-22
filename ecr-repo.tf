d3c202d0facb7fba8ba8d3daa449bffb33827574
nsaxelby <mail@nsaxelby.com>
2023-06-22 14:57:45 +0100

    Get rid of fluentbit

--- ecr-repo.tf
+++ ecr-repo.tf
@@ -29,40 +29,4 @@ resource "aws_ecr_lifecycle_policy" "default_policy" {
     ]
 }
 EOF
-
-}
-
-
-resource "aws_ecr_repository" "my-ecr-repo-fluent" {
-  name         = "my-ecr-repo-fluent"
-  force_delete = true
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-
-resource "aws_ecr_lifecycle_policy" "default_policy_fluent" {
-  repository = aws_ecr_repository.my-ecr-repo-fluent.name
-
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Keep only the last 2 untagged images.",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "imageCountMoreThan",
-                "countNumber": 2
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-
 }
--- task-definition.tf
+++ task-definition.tf
@@ -11,8 +11,8 @@ resource "aws_ecs_task_definition" "my-task-definition" {
     {
       name      = "envoy"
       image     = "${aws_ecr_repository.my-ecr-repo.repository_url}:latest"
-      cpu       = 480
-      memory    = 960
+      cpu       = 512
+      memory    = 1024
       essential = true
       portMappings = [
         {
@@ -21,7 +21,13 @@ resource "aws_ecs_task_definition" "my-task-definition" {
         }
       ]
       logConfiguration = {
-        logDriver = "awsfirelens"
+        logDriver = "awslogs"
+        options = {
+          awslogs-create-group  = "true"
+          awslogs-group         = "awslogs-ecs"
+          awslogs-region        = "eu-west-1"
+          awslogs-stream-prefix = "awslogs-envoy"
+        }
       }
     },
     {
@@ -45,27 +51,7 @@ resource "aws_ecs_task_definition" "my-task-definition" {
           awslogs-stream-prefix = "awslogs-httpbin"
         }
       }
-    },
-    {
-      essential = true,
-      image     = "${aws_ecr_repository.my-ecr-repo-fluent.repository_url}:latest"
-      cpu       = 32
-      memory    = 64
-      name      = "log_router"
-      firelensConfiguration = {
-        type = "fluentbit"
-      }
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = "awslogs-ecs"
-          awslogs-region        = "eu-west-1"
-          awslogs-create-group  = "true"
-          awslogs-stream-prefix = "firelens"
-        }
-      }
-      memoryReservation = 50
     }
   ])
-  depends_on = [null_resource.docker_packaging, null_resource.docker_packaging_fluentbit]
+  depends_on = [null_resource.docker_packaging]
 }
