fe8b9b30bcfc5da9e419528de4defb96507ba574
nsaxelby <mail@nsaxelby.com>
2023-06-27 22:22:59 +0100

    Added k6 status and another notebook paragraph

--- k6.js
+++ k6.js
@@ -5,7 +5,10 @@ export const options = {
 };
 
 export default function () {
-  const response =
-    http.get("http://my-nlb-542a7ae420daefa1.elb.eu-west-1.amazonaws.com/json");
+  var address = "http://my-nlb-542a7ae420daefa1.elb.eu-west-1.amazonaws.com"
+  http.get(address + "/json");
+  http.get(address + "/status/500");
+  http.get(address + "/status/404");
+  http.get(address + "/status/404");
   sleep(1);
 }
