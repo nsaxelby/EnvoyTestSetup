8dbd299a516646fcdb29253b7ff93da819ed91cc
nsaxelby <mail@nsaxelby.com>
2023-10-31 18:58:56 +0000

    Add access logs aws (#4)

--- s3.tf
+++ s3.tf
@@ -6,3 +6,15 @@ resource "aws_s3_bucket" "flink-apps" {
     Name = "flink-apps"
   }
 }
+
+module "aws_logs" {
+  source = "trussworks/logs/aws"
+  version = "~> 16.0"
+  s3_bucket_name = "my-log-bucket-nlb"
+  default_allow = false
+  allow_alb = true
+  allow_nlb = true
+  create_public_access_block = true
+  versioning_status = "Enabled"
+  force_destroy = true
+}
